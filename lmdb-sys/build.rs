f691fc8360c1f47605157b32c7a6f0baab2d391f
leavehouse <heavelouse@gmail.com>
2017-10-19 14:37:13 -0500

    Change lmdb-sys to use latest gcc 0.3 release

--- lmdb-sys/build.rs
+++ lmdb-sys/build.rs
@@ -11,7 +11,7 @@ fn main() {
     lmdb.push("liblmdb");
 
     if !pkg_config::find_library("liblmdb").is_ok() {
-        gcc::Config::new()
+        gcc::Build::new()
                     .file(lmdb.join("mdb.c"))
                     .file(lmdb.join("midl.c"))
                     // https://github.com/LMDB/lmdb/blob/LMDB_0.9.21/libraries/liblmdb/Makefile#L25
